b963e9a08878f53ab4e05fe43fd3a80338850d0e
mia <55366664+imcodding@users.noreply.github.com>
2022-04-21 15:32:49 +0900

    [part2] 회원 도메인 - Service

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복 회원 검증
+     */
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+        /*
+        같은 이름을 가진 회원이 동시에 가입하면 문제 발생할 수 있음.
+        => 이를 처리하기 위해서 '이름' 에 유니크 인덱스를 걸어주는 게 좋음.
+         */
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 조회
+     */
+    public Member findMember(Long id) {
+        return memberRepository.findOne(id);
+    }
+}
