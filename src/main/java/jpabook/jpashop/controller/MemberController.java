133eed47e0936a1df114bc62611151ecffbd1d8d
imcodding <suy0226@naver.com>
2022-04-24 15:29:58 +0900

    [part3] 회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -40,4 +41,18 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
     }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        // Member 엔티티로 바로 데이터를 받기 보다, dto(화면에 맞는 형태) 를 따로 만들어서 하는 것 권장.
+        // 현재는 간단하고 Member 에 따로 변경 포인트가 없기 때문에 Member 로 받음.
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+    /*
+     API 를 만들 때는 절대 Entity 를 외부로 반환하면 안 된다.
+     - 패스워드를 필드를 추가할 경우, 노출될 가능성 있음
+     - 필드가 추가됨에 따라 api 스팩이 변경되게 됨.
+     */
 }
